68f7251417f0dc50cac38b198d64f2cb8ac6951a
Amar Chandan <78189163+amarchandan@users.noreply.github.com>
2024-10-18 20:16:15 +0530

    Create Multiplication_tables.c

---
+++ Multiplication_tables.c
@@ -0,0 +1,22 @@
+// first Exercise in c
+
+#include<stdio.h>
+
+int main (){
+    int get_num; // save users enter num
+    printf("Enter your number : ");//ask users input
+    scanf("%d",&get_num);// save users input
+    printf("here your answer\n");//starting of result
+    printf("your num : %d\n",get_num);
+    printf("%d\n",get_num*1);
+    printf("%d\n",get_num*2);
+    printf("%d\n",get_num*3);
+    printf("%d\n",get_num*4);
+    printf("%d\n",get_num*5);
+    printf("%d\n",get_num*6);
+    printf("%d\n",get_num*7);
+    printf("%d\n",get_num*8);
+    printf("%d\n",get_num*9);
+    printf("%d\n",get_num*10);
+
+}
